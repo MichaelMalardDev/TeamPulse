5e5cfb5c6f05e6402e143876888ecfb062540c57
Michaël Malard <michaelmalard04@gmail.com>
2025-07-22 03:07:59 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TeamOverview>
    
    wi

--- src/components/dashboard/team-overview.tsx
+++ src/components/dashboard/team-overview.tsx
@@ -15,7 +15,7 @@ export default function TeamOverview({ team }: TeamOverviewProps) {
       transition={{ duration: 0.5, delay: 0.5 }}
     >
       <h2 className="text-xl font-semibold">Team Overview</h2>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <AnimatePresence>
           {team.map((member, index) => (
             <motion.div
